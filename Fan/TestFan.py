767f708f7c52be8ae2305a28412dc828bd71e581
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-07-02 19:30:03 +0800

    pseudocode

---
+++ Fan/TestFan.py
@@ -0,0 +1,21 @@
+# Ralph Lorenz I. Codilan
+# BSCpE 1-5
+# Object-Oriented Programming | Car Class
+
+# Pseudocode
+Class TestFan:
+    Main:
+        fan1 = Fan(speed=fan.FAST, radius=10, color="yellow", on=True)
+        fan2 = Fan(speed=fan.MEDIUM, radius=5, color="blue", on=False)
+
+        Print("Fan 1:")
+        Print("Speed: ", fan1.speedGetter())
+        Print("Radius: ", fan1.radiusGetter())
+        Print("Color: ", fan1.colorGetter())
+        Print("On: ", fan1.onGetter())
+
+        Print("Fan 2:")
+        Print("Speed: ", fan2.speedGetter())
+        Print("Radius: ", fan2.radiusGetter())
+        Print("Color: ", fan2.colorGetter())
+        Print("On: ", fan2.onGetter())
