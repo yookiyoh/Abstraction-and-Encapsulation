d273061217853668dfa3ed94dcc1eb84796c6ee3
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-07-03 11:24:08 +0800

    pseudocode

--- Car/Car.py
+++ Car/Car.py
@@ -2,3 +2,29 @@
 # BSCpE 1-5
 # Object-Oriented Programming | Car Class
 
+# Pseudocode
+
+'''
+class Car:
+    __year_model
+    __maker
+    __speed
+    
+    method __init__(year_model, make):
+        __year_model = year_model
+        __make = make
+        __speed = 0
+    
+    method accelerate():
+        __speed += 5
+    
+    method brake(): 
+        if __speed >= 5:
+            __speed -= 5
+        else:
+           __speed = 0
+
+    method get_speed():
+        return __speed
+'''
+
--- Car/TestCar.py
+++ Car/TestCar.py
@@ -2,3 +2,61 @@
 # BSCpE 1-5
 # Object-Oriented Programming | Car Class
 
+# Pseudocode
+
+'''
+import time
+import tkinter as tk
+from tkinter import messagebox
+from tqdm import tqdm
+from Car import Car
+
+method accelerate_car(car):
+    car.accelerate()
+    time.sleep(0.5)
+
+method brake_car(car):
+    car.brake()
+    time.sleep(0.5)
+
+method main():
+    car = Car(2023, "Exampler Maker")
+
+    progress_bar = create_tqdm(total=10, desc="Accelerating and Braking", unit="step")
+
+    window = create_tkinter_window(title="Car Acceleration")
+
+    speed_label = create_tkinter_label(text="Current Speed:")
+    speed_label.pack()
+
+    speed_value = create_tkinter_label(text=car.get_speed())
+    speed_value.pack()
+
+    try:
+        loop 5 times:
+            accelerate_car(car)
+            update_speed_label(speed_value, car.get_speed())
+            update_progress_bar(progress_bar)
+            update_window(window)
+
+        loop 5 times:
+            brake_car(car)
+            update_speed_label(speed_value, car.get_speed())
+            update_progress_bar(progress_bar)
+            update_window(window)
+
+        update_speed_label(speed_value, 0)
+        update_progress_bar(progress_bar)
+        update_window(window)
+        sleep(0.5)
+
+    except Exception as e:
+        show_error_message("Error", e)
+
+    finally:
+        close_progress_bar(progress_bar)
+        destroy_window(window)
+
+if __name__ == '__main__'
+    main()
+'''
