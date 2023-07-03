6673333cb695522169e37e61246a8b87def4222a
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-07-03 09:23:37 +0800

    created method that displays the fan properties

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -78,6 +78,17 @@ class TestFan:
         on = input("Turn on fan? (y/n): ")   # Get on/off input as a string
         return on.lower() == 'y'   # Return True if 'y' is entered, False otherwise
     
-    
+    # Method that displays the fan properties
+    def display_fan_properties(self, fan):
+        print("\nLoading info...\n")   # Print loading message
+        for i in tqdm(range(100)):   # Iterate over a range with tqdm progress bar
+            time.sleep(0.01)   # Sleep for a short duration
+
+        time.sleep(2)   # Sleep for 2 seconds
+        print("\n[Fan Info Loaded Successfully!]\n")   # Print success message
+        print("Fan Speed:", fan.get_speed())   # Print fan speed
+        print("Fan Radius:", fan.get_radius())   # Print fan radius 
+        print("Fan Color:", fan.get_color())   # Print fan color 
+        print(fan.display_message())   # Print fan message (spinning or off)
+        print()
         
-    
